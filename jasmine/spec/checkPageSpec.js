4a8047b28787a05c151469c497ab4caa297828f7
Steven Senkus <steven.senkus@yahoo.com>
2013-10-27 15:37:43 -0700

    running some specs!

--- jasmine/spec/checkPageSpec.js
+++ jasmine/spec/checkPageSpec.js
@@ -1,28 +1,27 @@
-function checkPage() {
-    // are breadcrumbs on this page?
-    var bcWrapper = $('#c4-breadcrumbs-id10T');
-    // is this a product page? does page have breadcrumb wrapper?            
-    var bcs = $('.breadcrumbs');
-    if (bcWrapper.length > 0 || bcs.length > 0) {
-        console.log('breadcrumbs are present!');
-        bcPresence = true;
-    } else {
-        console.log('breadcrumbs are not present!');
-        bcPresence = false;
-    }
-    return bcPresence;
-}
+/*function checkPage() {
+ // are breadcrumbs on this page?
+ var bcWrapper = $('#c4-breadcrumbs-id10T');
+ // is this a product page? does page have breadcrumb wrapper?            
+ var bcs = $('.breadcrumbs');
+ if (bcWrapper.length > 0 || bcs.length > 0) {
+ console.log('breadcrumbs are present!');
+ bcPresence = true;
+ } else {
+ console.log('breadcrumbs are not present!');
+ bcPresence = false;
+ }
+ return bcPresence;
+ }*/
 
 describe("checkPage", function() {
-    it("identifies a breadcrumb-trail-enabled page", function() {
-        
-        
-        
-        
-        
-        
-        expect();
+    it("should return true if the page contains the # element", function() {
+
+        // == SETUP == //
+        jasmine.getFixtures().fixturesPath = 'fixtures';
+        loadFixtures('home.html');
+       
+        expect($('#home')).toHaveText('home page');
     });
-    
-    
+
+
 });
--- jasmine/spec/jqueryCookiePluginSpec.js
+++ jasmine/spec/jqueryCookiePluginSpec.js
@@ -1,6 +1,42 @@
 describe("jqueryCookie", function() {
     it("should set an object as a cookie value as a JSON string", function() {
-        $.cookie('bcCookie', {"test":"testValue"});
-        expect($.cookie('bcCookie')).toEqual({"test":"testValue"});
+
+        // == SETUP == //
+        jasmine.getFixtures().fixturesPath = 'fixtures';
+        loadFixtures('home.html');
+        
+        //$.cookie.raw = true;
+        $.cookie.json = true;
+        var testObj = {
+            "test": "testValue"
+        };
+        
+        console.log($.cookie('bcCookie'));
+        
+        $.cookie('bcCookie', testObj);
+        //expect($.cookie('bcCookie')).toEqual("{\"test\":\"testValue\"}");
+        expect($.cookie('bcCookie')).toEqual(testObj);
     });
+    
+    it("should set an object as a cookie value as a raw string", function() {
+
+        // == SETUP == //
+        jasmine.getFixtures().fixturesPath = 'fixtures';
+        loadFixtures('home.html');
+        
+        $.cookie.raw = true;
+        //$.cookie.json = true;
+        var testObj = {
+            "test": "testValue"
+        };
+        
+        console.log($.cookie('bcCookie'));
+        
+        $.cookie('bcCookie', testObj);
+        expect($.cookie('bcCookie')).toEqual("{\"test\":\"testValue\"}");
+        
+    });    
+    
 });
+
+
